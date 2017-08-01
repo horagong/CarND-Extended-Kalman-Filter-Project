5410b6878d87a4f1bd29517cca4f34e3610cea01
horagong <horagong@gmail.com>
2017-08-01 18:16:45 +0900

    init commit

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,7 +1,9 @@
 #include "kalman_filter.h"
+#include <iostream>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
+using namespace std;
 
 KalmanFilter::KalmanFilter() {}
 
@@ -19,21 +21,69 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 
 void KalmanFilter::Predict() {
   /**
-  TODO:
+  DONE:
     * predict the state
   */
+  x_ = F_ * x_;
+	MatrixXd Ft = F_.transpose();
+	P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
-  TODO:
+  DONE:
     * update the state by using Kalman Filter equations
   */
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd K = P_ * Ht * Si;
+  VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
-  TODO:
+  DONE:
     * update the state by using Extended Kalman Filter equations
   */
+
+  MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd K = P_ * Ht * Si;
+
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+  float ro = sqrt(px*px + py*py);
+  float phi = atan2(py, px);
+  float ro_dot = 0;
+  
+  if (fabs(ro) < 0.0001) {
+    cout << "UpdateEKF ro=zero" << endl;
+  } else {
+    ro_dot = (px*vx + py*vy)/ro;
+  }
+  
+  VectorXd z_pred = VectorXd(3);
+  z_pred << ro, phi, ro_dot;
+	VectorXd y = z - z_pred;
+  while (y(1) < -M_PI) {
+    y(1) += 2 * M_PI;
+  }
+  while (y(1) > M_PI) {
+    y(1) -= 2 * M_PI;
+  }
+
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
 }
